6ce204e3e4ff166be50bbc3655b0e4e8dd7abd47
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2018-01-02 17:49:38 +0100

    Initial Commit

---
+++ DocuTest/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  DocuTest
+//
+//  Created by Nicola Ferruzzi on 02/01/2018.
+//  Copyright © 2018 Nicola Ferruzzi. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
